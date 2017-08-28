2c5de99e9dacdab822ef16bddef8070d0a12e6e9
Linda  Vasquez <lindavasquez@Lindas-MacBook-Pro.local>
2017-08-28 17:02:09 -0600

    Clean code

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
 var zipCode = $("#Zipcode")[0];
-var map= $("iframe")[0];
+var map = $("iframe")[0];
 console.log(map);
- zipCode.addEventListener("change", function(e){
-   console.log('test');
-   map.src='https://www.google.com/maps/embed/v1/search?q=Planned+Parenthood+' + zipCode.textContent + '&key=AIzaSyA4IrLIm6WYjQmw2-OML_bwFF1KQEUBQYE';
+zipCode.addEventListener("change", function(e) {
+  console.log('test');
+  map.src = 'https://www.google.com/maps/embed/v1/search?q=Planned+Parenthood+' + zipCode.textContent + '&key=AIzaSyA4IrLIm6WYjQmw2-OML_bwFF1KQEUBQYE';
 
- })
+})
